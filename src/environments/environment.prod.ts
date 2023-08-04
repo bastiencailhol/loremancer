e80d03869eb87961139813c67263dd2ca60db526
Bastien Cailhol <bastien.cailhol@gmail.com>
2023-08-04 19:17:18 +0200

    update firebase cache

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
-};
+  production: true,
+  // firebase: {
+  //   apiKey: 'AIzaSyD-_a8EPPIwPHKctBHQjUnFfAE4vsJSY_c',
+  //   authDomain: 'sparkle-hero.firebaseapp.com',
+  //   projectId: 'sparkle-hero',
+  //   storageBucket: 'sparkle-hero.appspot.com',
+  //   messagingSenderId: '538183133998',
+  //   appId: '1:538183133998:web:f84a5700ee79a6f26128bf',
+  //   measurementId: 'G-LVB8W5Q97W',
+  // },
+}
