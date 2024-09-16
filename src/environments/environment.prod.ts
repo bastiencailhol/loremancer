478e65bb32a81881faf1caac5f227dd800975ecf
Bastien Cailhol <bastien.cailhol@gmail.com>
2024-09-16 15:48:58 +0200

    build into prod with rebranding

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,11 +2,13 @@ export const environment = {
   production: true,
   // firebase: {
   // apiKey: "AIzaSyAOGOjIdcYG8UQVYLICtJmhwu1_pdz3yrc",
-  // authDomain: "lore-draft.firebaseapp.com",
-  // projectId: "lore-draft",
-  // storageBucket: "lore-draft.appspot.com",
+  // authDomain: "lore-mancer.firebaseapp.com",
+  // projectId: "lore-mancer",
+  // storageBucket: "lore-mancer.appspot.com",
   // messagingSenderId: "146418471698",
   // appId: "1:146418471698:web:51d0ae3cf53b53600399cb",
   // measurementId: "G-Y4N2LCZP0S"
   // },
 }
+
+export const imageReferencesRootPath = 'assets/img/image-references'
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   // firebase: {
   // apiKey: "AIzaSyAOGOjIdcYG8UQVYLICtJmhwu1_pdz3yrc",
-  // authDomain: "lore-draft.firebaseapp.com",
-  // projectId: "lore-draft",
-  // storageBucket: "lore-draft.appspot.com",
+  // authDomain: "lore-mancer.firebaseapp.com",
+  // projectId: "lore-mancer",
+  // storageBucket: "lore-mancer.appspot.com",
   // messagingSenderId: "146418471698",
   // appId: "1:146418471698:web:51d0ae3cf53b53600399cb",
   // measurementId: "G-Y4N2LCZP0S"
