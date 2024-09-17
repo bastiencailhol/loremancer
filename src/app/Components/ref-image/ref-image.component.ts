b6da56f5ecb1b7edca3b51c52d5eda8aea773ea8
Bastien Cailhol <bastien.cailhol@gmail.com>
2024-09-17 16:21:09 +0200

    image height animation now waiting for image to load

--- src/app/Components/ref-image/ref-image.component.ts
+++ src/app/Components/ref-image/ref-image.component.ts
@@ -6,8 +6,6 @@ import {
   Output,
   ViewChild,
   ElementRef,
-  AfterViewInit,
-  OnChanges,
 } from '@angular/core'
 
 @Component({
@@ -16,47 +14,37 @@ import {
   styleUrls: ['./ref-image.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RefImageComponent implements AfterViewInit, OnChanges {
-  @ViewChild('expandable', { static: false }) expandable!: ElementRef // Assure-toi que expandable est de type ElementRef
-  @Output() onClick = new EventEmitter()
-  @Input() src: string
+export class RefImageComponent {
+  @ViewChild('expandable', { static: false }) expandable!: ElementRef
+  @Output() onClick = new EventEmitter<void>()
+  @Input() src: string = ''
   firstLoad = true
-
-  ngAfterViewInit() {
-    this.updateHeight() // Appel initial dans ngAfterViewInit
-  }
-
-  ngOnChanges() {
-    if (this.expandable) {
-      this.updateHeight() // Appel uniquement si expandable est défini
-    }
-  }
 
   onImageClick() {
     this.onClick.emit()
   }
 
-  updateHeight(
-    delay = 50 /* valeur arbitraire pour attendre les images. Risque de ne plus fonctionner en ligne à cause du délai de DL)*/,
-  ) {
+  updateHeight() {
     const el = this.expandable.nativeElement
 
-    setTimeout(() => {
+    requestAnimationFrame(() => {
       let prevHeight
       if (this.firstLoad) {
         el.style.height = '0px'
         prevHeight = '0px'
       } else {
-        prevHeight = this.firstLoad ? '0px' : el.style.height
+        prevHeight = el.style.height
         el.style.height = 'auto'
       }
       const newHeight = el.scrollHeight + 'px'
       el.style.height = prevHeight
 
-      setTimeout(() => {
+      // Use requestAnimationFrame for smoother animation
+      requestAnimationFrame(() => {
         el.style.height = newHeight
-      }, 50)
-    }, delay)
+      })
+    })
+
     this.firstLoad = false
   }
 }
